045a80f1b90b1a2e3182c7a74d3d4857aea65a05
Thomas Oltmann <thomas.oltmann.hhg@gmail.com>
2021-08-11 15:03:56 +0200

    Nicer alignment & indentation.

--- dh_cuts.h
+++ dh_cuts.h
@@ -36,7 +36,7 @@
 #include <stdio.h>
 
 struct dh_branch_saves_ {
-	int saved_depth;
+	int   saved_depth;
 	void *saved_jump;
 };
 
@@ -45,17 +45,18 @@ void dh_init(FILE *pipe);
 void dh_summarize(void);
 
 void dh_push(char const *format, ...);
-void dh_pop(void);
+void dh_pop (void);
 
-#define dh_branch(code) { \
-		struct dh_branch_saves_ s; \
-		sigjmp_buf my_jmp; \
-		int signal = sigsetjmp(my_jmp, 1); \
+#define dh_branch(code) {                            \
+		struct dh_branch_saves_ s;           \
+		sigjmp_buf              my_jmp;      \
+		int                     signal;      \
+		signal = sigsetjmp(my_jmp, 1);       \
 		dh_branch_beg_(signal, &my_jmp, &s); \
-		if (!signal) { \
-			code \
-		} \
-		dh_branch_end_(&s); \
+		if (!signal) {                       \
+			code                         \
+		}                                    \
+		dh_branch_end_(&s);                  \
 	}
 
 #ifndef DH_OPTION_EPSILON
@@ -63,19 +64,19 @@ void dh_pop(void);
 #endif
 
 #define dh_throw(format, ...) dh_throw_(__LINE__, format, __VA_ARGS__)
-#define dh_assert(cond) dh_assert_(__LINE__, cond, #cond)
-#define dh_assertiq(a, b) dh_assertiq_(__LINE__, a, b, #a "==" #b)
-#define dh_assertfq(a, b) dh_assertfq_(__LINE__, a, b, DH_OPTION_EPSILON, #a "==" #b)
-#define dh_assertsq(a, b) dh_assertsq_(__LINE__, a, b, #a "==" #b)
-#define dh_asserteq(a, b, e) dh_assertfq_(__LINE__, a, b, e, #a "==" #b)
+#define dh_assert(cond)       dh_assert_(__LINE__, cond, #cond)
+#define dh_assertiq(a, b)     dh_assertiq_(__LINE__, a, b, #a "==" #b)
+#define dh_assertfq(a, b)     dh_assertfq_(__LINE__, a, b, DH_OPTION_EPSILON, #a "==" #b)
+#define dh_assertsq(a, b)     dh_assertsq_(__LINE__, a, b, #a "==" #b)
+#define dh_asserteq(a, b, e)  dh_assertfq_(__LINE__, a, b, e, #a "==" #b)
 
 /* internal functions that have to be visible.
  * do not call these directly. */
-void dh_throw_(int ln, char const *format, ...);
-void dh_assert_(int ln, int cond, char const *str);
-void dh_assertiq_(int ln, long long a, long long b, char const *str);
-void dh_assertfq_(int ln, double a, double b, double e, char const *str);
-void dh_assertsq_(int ln, char const *a, char const *b, char const *str);
+void dh_throw_     (int ln, char const *format, ...);
+void dh_assert_    (int ln, int cond, char const *str);
+void dh_assertiq_  (int ln, long long a, long long b, char const *str);
+void dh_assertfq_  (int ln, double a, double b, double e, char const *str);
+void dh_assertsq_  (int ln, char const *a, char const *b, char const *str);
 void dh_branch_beg_(int signal, sigjmp_buf *my_jmp, struct dh_branch_saves_ *s);
 void dh_branch_end_(struct dh_branch_saves_ *s);
 
@@ -97,7 +98,7 @@ void dh_branch_end_(struct dh_branch_saves_ *s);
 # define TEXT_ARROW "\u2190"
 # define TEXT_LINE  "\u2500\u2500"
 
-#endif
+#endif /* DH_OPTION_ASCII_ONLY */
 
 #include <stdlib.h>
 #include <stdarg.h>
@@ -106,47 +107,48 @@ void dh_branch_end_(struct dh_branch_saves_ *s);
 #include <signal.h>
 
 #define MAX_NAME_LENGTH 200
-#define MAX_DEPTH 50
+#define MAX_DEPTH       50
+#define NO_LINENO       -1
 
-enum { FAIL, CRASH };
+enum { FAIL,  CRASH  };
 enum { THROW, ASSERT };
-
-#define NO_LINENO -1
 
 static int const dh_caught_signals[] = { SIGILL, SIGFPE, SIGSEGV, SIGBUS, SIGSYS, SIGPIPE, 0 };
 
 struct dh_this {
 	sigjmp_buf *crash_jump;
-	int stack_depth;
+	int         stack_depth;
 	char const *stack[MAX_DEPTH];
 };
 
 struct dh_sink {
 	FILE *pipe;
-	int print_depth;
-	int error_count;
-	int crash_count;
+	int   print_depth;
+	int   error_count;
+	int   crash_count;
 };
 
 static struct dh_this dh_this;
 static struct dh_sink dh_sink;
 
-static char const *dh_name_of_signal_(int signal)
+static char const *
+dh_name_of_signal_(int signal)
 {
 	switch (signal) {
 		case SIGILL:  return "illegal instruction (SIGILL)";  break;
-		case SIGFPE:  return "arithmetic exception (SIGFPE)";  break;
-		case SIGSEGV: return "segmentation fault (SIGSEGV)"; break;
-		case SIGBUS:  return "bus error (SIGBUS)";  break;
+		case SIGFPE:  return "arithmetic exception (SIGFPE)"; break;
+		case SIGSEGV: return "segmentation fault (SIGSEGV)";  break;
+		case SIGBUS:  return "bus error (SIGBUS)";            break;
 		case SIGSYS:  return "illegal system call (SIGSYS)";  break;
-		case SIGPIPE: return "broken pipe (SIGPIPE)"; break;
+		case SIGPIPE: return "broken pipe (SIGPIPE)";         break;
 		/* the default path should never be taken,
 		 * as only the above signals are actually caught. */
-		default: return "unknown signal"; break;
+		default:      return "unknown signal";                break;
 	}
 }
 
-static void dh_signal_handler_(int signal)
+static void
+dh_signal_handler_(int signal)
 {
 	if (dh_this.crash_jump != NULL) {
 		if (signal == SIGFPE) {
@@ -163,19 +165,22 @@ static void dh_signal_handler_(int signal)
 	}
 }
 
-void dh_init(FILE *pipe)
+void
+dh_init(FILE *pipe)
 {
 	dh_sink.pipe = pipe;
+
 	struct sigaction action;
-	memset(&action, 0, sizeof(struct sigaction));
+	memset(&action, 0, sizeof action);
 	action.sa_handler = dh_signal_handler_;
 	sigemptyset(&action.sa_mask);
-	for (int i = 0; dh_caught_signals[i] != 0; ++i) {
+	for (int i = 0; dh_caught_signals[i] != 0; i++) {
 		sigaction(dh_caught_signals[i], &action, NULL);
 	}
 }
 
-void dh_summarize(void)
+void
+dh_summarize(void)
 {
 #if !DH_OPTION_PEDANTIC
 	if (dh_sink.error_count != 0 || dh_sink.crash_count != 0)
@@ -187,7 +192,8 @@ void dh_summarize(void)
 	}
 }
 
-void dh_push(char const *format, ...)
+void
+dh_push(char const *format, ...)
 {
 	char *str = malloc(MAX_NAME_LENGTH);
 
@@ -199,38 +205,40 @@ void dh_push(char const *format, ...)
 	dh_this.stack[dh_this.stack_depth++] = str;
 }
 
-void dh_pop(void)
+void
+dh_pop(void)
 {
-	free((char *)dh_this.stack[--dh_this.stack_depth]);
+	free((char *) dh_this.stack[--dh_this.stack_depth]);
 	if (dh_sink.print_depth > dh_this.stack_depth)
 		dh_sink.print_depth = dh_this.stack_depth;
 }
 
-static void dh_print_nesting_(int depth)
+static void
+dh_print_nesting_(int depth)
 {
-	int i;
-	for (i = 0; i < depth; ++i)
+	for (int i = 0; i < depth; ++i)
 		fputs(TEXT_DOTS, dh_sink.pipe);
 	fputs(TEXT_HIER, dh_sink.pipe);
 }
 
-static void dh_report_(int kind, int signal, int ln, char const *msg)
+static void
+dh_report_(int kind, int signal, int ln, char const *msg)
 {
 	char const *kind_name, *signal_name;
 	switch (kind) {
-		case FAIL:
-			++dh_sink.error_count;
-			kind_name = "FAIL";
-			switch (signal) {
-				case THROW: signal_name = "throw"; break;
-				case ASSERT: signal_name = "assert"; break;
-			}
-			break;
-		case CRASH:
-			++dh_sink.crash_count;
-			kind_name = "CRASH";
-			signal_name = dh_name_of_signal_(signal);
-			break;
+	case FAIL:
+		dh_sink.error_count++;
+		kind_name = "FAIL";
+		switch (signal) {
+			case THROW:  signal_name = "throw";  break;
+			case ASSERT: signal_name = "assert"; break;
+		}
+		break;
+	case CRASH:
+		dh_sink.crash_count++;
+		kind_name   = "CRASH";
+		signal_name = dh_name_of_signal_(signal);
+		break;
 	}
 
 	int depth = dh_sink.print_depth;
@@ -238,7 +246,7 @@ static void dh_report_(int kind, int signal, int ln, char const *msg)
 		dh_print_nesting_(depth);
 		fputs(dh_this.stack[depth], dh_sink.pipe);
 		fputs("\n", dh_sink.pipe);
-		++depth;
+		depth++;
 	}
 	dh_sink.print_depth = dh_this.stack_depth;
 	dh_print_nesting_(dh_sink.print_depth);
@@ -252,27 +260,31 @@ static void dh_report_(int kind, int signal, int ln, char const *msg)
 	fprintf(dh_sink.pipe, "\t\t" TEXT_ARROW " %s\n", kind_name);
 }
 
-void dh_branch_beg_(int signal, sigjmp_buf *my_jmp, struct dh_branch_saves_ *s)
+void
+dh_branch_beg_(int signal, sigjmp_buf *my_jmp, struct dh_branch_saves_ *s)
 {
 	if (signal) {
 		dh_report_(CRASH, signal, NO_LINENO, NULL);
 	} else {
-		*s = (struct dh_branch_saves_){dh_this.stack_depth, (void *)dh_this.crash_jump};
+		*s = (struct dh_branch_saves_) { dh_this.stack_depth, (void *) dh_this.crash_jump };
 		dh_this.crash_jump = my_jmp;
 	}
 }
 
-void dh_branch_end_(struct dh_branch_saves_ *s)
+void
+dh_branch_end_(struct dh_branch_saves_ *s)
 {
 	dh_this.crash_jump = s->saved_jump;
 	/* restore the stack in case of a crash.
 	 * also helps recovering from missing dh_pop()'s,
 	 * though you *really* shouldn't rely on this behaviour. */
-	while (dh_this.stack_depth > s->saved_depth)
+	while (dh_this.stack_depth > s->saved_depth) {
 		dh_pop();
+	}
 }
 
-void dh_throw_(int ln, char const *format, ...)
+void
+dh_throw_(int ln, char const *format, ...)
 {
 	char *str = malloc(MAX_NAME_LENGTH);
 
@@ -286,17 +298,20 @@ void dh_throw_(int ln, char const *format, ...)
 	free(str);
 }
 
-void dh_assert_(int ln, int cond, char const *str)
+void
+dh_assert_(int ln, int cond, char const *str)
 {
 	if (!cond) dh_report_(FAIL, ASSERT, ln, str);
 }
 
-void dh_assertiq_(int ln, long long a, long long b, char const *str)
+void
+dh_assertiq_(int ln, long long a, long long b, char const *str)
 {
 	dh_assert_(ln, a == b, str);
 }
 
-void dh_assertfq_(int ln, double a, double b, double e, char const *str)
+void
+dh_assertfq_(int ln, double a, double b, double e, char const *str)
 {
 	/* because of the rounding behaviour of floating-point numbers, two expressions
 	 * that mathematically should evaluate to the same value can actually differ in
@@ -310,9 +325,10 @@ void dh_assertfq_(int ln, double a, double b, double e, char const *str)
 	dh_assert_(ln, d <= e, str);
 }
 
-void dh_assertsq_(int ln, char const *a, char const *b, char const *str)
+void
+dh_assertsq_(int ln, char const *a, char const *b, char const *str)
 {
 	dh_assert_(ln, strcmp(a, b) != 0, str);
 }
 
-#endif
+#endif /* DH_IMPLEMENT_HERE */
